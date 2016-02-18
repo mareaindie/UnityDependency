dbf25a4b9d001d468cde209461a4cc02f666e6b3
mareaindie <mareaindie@gmail.com>
2016-02-18 21:05:03 +0100

    Create build.sh
    Execute automated tests in Travis CI.

---
+++ Scripts/build.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+
+# Change this the name of your project. This will be the name of the final executables as well.
+project="ci-build"
+
+echo "Attempting to run $project tests"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -runEditorTests \
+  -editorTestsResultFile $(pwd)/tests.log
+
+echo 'Logs from tests'
+cat $(pwd)/tests.log
